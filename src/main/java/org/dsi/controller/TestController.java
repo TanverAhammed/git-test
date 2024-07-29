72c6785c3c1059031f067975caa9926d14954360
dsi <tanvermbstuit16@gmail.com>
2024-07-29 12:47:51 +0600

    git-test initiate

---
+++ src/main/java/org/dsi/controller/TestController.java
@@ -0,0 +1,16 @@
+package org.dsi.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping
+    @ResponseBody
+    public String test() {
+        return "test demo";
+    }
+
+}
